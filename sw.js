46264a01f3336e21d8408b5c441d42f4045bdaab
friedrith <4005226+friedrith@users.noreply.github.com>
2021-02-16 02:48:07 +0000

    Deploying to gh-pages from  @ 81f52a2c64e4b03b7589bf80d57ccb9f6cde903e 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f286705e830077d642f6.js"
+    "url": "webpack-runtime-3847cc973126768ff053.js"
   },
   {
     "url": "framework-d585a04b7d285a7760d6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1662be6de8fc72fbd181425de2ac6e39"
+    "revision": "3725dfc1fd9b4aff6a02a100e771d215"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-19245c8506e49b502b12.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "61a7adecaf0edfab9144624e7d95042b"
+    "revision": "ccc16dfc6746ed62ca4aa58fb33e1154"
   },
   {
     "url": "polyfill-d2a12da121185e869b24.js"
